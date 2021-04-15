480575bad0c682bc6c30c8a96e14fb354408c4c6
Dragon44PL <krystian.pieniak44@gmail.com>
2021-04-15 13:17:58 +0200

    init

---
+++ src/test/java/com/github/projectdetector/demo/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.projectdetector.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
