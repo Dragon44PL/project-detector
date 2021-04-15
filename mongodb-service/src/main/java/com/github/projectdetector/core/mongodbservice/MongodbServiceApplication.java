6e78032aa7b7c860cf18b8cbaed04126aa2f7210
Dragon44PL <krystian.pieniak44@gmail.com>
2021-04-15 13:46:56 +0200

    Created Mongodb-service module

---
+++ mongodb-service/src/main/java/com/github/projectdetector/core/mongodbservice/MongodbServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.projectdetector.core.mongodbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbServiceApplication.class, args);
+	}
+
+}
