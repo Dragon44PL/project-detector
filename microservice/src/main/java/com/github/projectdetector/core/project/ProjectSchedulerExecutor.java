845f052b854a1a617fb69bafbb5f5196f0851c93
Dragon44PL <krystian.pieniak44@gmail.com>
2021-06-02 23:45:31 +0200

    Added "isRunning" variable and method, Added "restart" method, changed "prepareProjectScheduler" method

--- microservice/src/main/java/com/github/projectdetector/core/project/ProjectSchedulerExecutor.java
+++ microservice/src/main/java/com/github/projectdetector/core/project/ProjectSchedulerExecutor.java
@@ -2,6 +2,7 @@ package com.github.projectdetector.core.project;
 
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 class ProjectSchedulerExecutor {
 
@@ -9,10 +10,12 @@ class ProjectSchedulerExecutor {
     private final ProjectScheduler projectScheduler;
     private SchedulerPeriodConfig schedulerPeriodConfig;
     private ScheduledFuture<?> scheduledFuture;
+    private boolean isRunning;
 
     ProjectSchedulerExecutor(ScheduledExecutorService scheduledExecutorService, ProjectScheduler projectScheduler) {
         this.scheduledExecutorService = scheduledExecutorService;
         this.projectScheduler = projectScheduler;
+        this.isRunning = false;
     }
 
     void setSchedulerPeriodConfig(SchedulerPeriodConfig schedulerPeriodConfig) {
@@ -20,15 +23,29 @@ class ProjectSchedulerExecutor {
     }
 
     private void prepareProjectScheduler() {
-        this.scheduledFuture = scheduledExecutorService.scheduleAtFixedRate(projectScheduler, schedulerPeriodConfig.getInitialDelay(), schedulerPeriodConfig.getPeriod(), schedulerPeriodConfig.getTimeUnit());
+        final long initialDelay = schedulerPeriodConfig.getInitialDelay();
+        final long period = schedulerPeriodConfig.getPeriod();
+        final TimeUnit timeUnit = schedulerPeriodConfig.getTimeUnit();
+        this.scheduledFuture = scheduledExecutorService.scheduleAtFixedRate(projectScheduler, initialDelay, period, timeUnit);
     }
 
     void stop() {
         scheduledFuture.cancel(true);
+        this.isRunning = false;
     }
 
     void start() {
         prepareProjectScheduler();
+        this.isRunning = true;
+    }
+
+    boolean isRunning() {
+        return this.isRunning;
+    }
+
+    void restart() {
+        stop();
+        start();
     }
 
 }
